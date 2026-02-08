47cc794afbbcf168b838772a3dc3b0b588be4c32
Leon Ihrig <lihrig@2-A-1.42heilbronn.de>
2026-02-08 12:08:16 +0100

    implementing fixed.cpp

--- ex00/fixed.cpp
+++ ex00/fixed.cpp
@@ -6,7 +6,45 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/08 11:33:29 by lihrig            #+#    #+#             */
-/*   Updated: 2026/02/08 11:33:30 by lihrig           ###   ########.fr       */
+/*   Updated: 2026/02/08 12:04:34 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "fixed.hpp"
+
+Fixed::Fixed() : _value(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& other)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = other;
+}
+
+Fixed& Fixed::operator=(const Fixed& other)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &other)
+    {
+        this->_value = other.getStoredValue();
+    }
+    return *this;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getStoredValue(void) const
+{
+    std::cout << "getStoredValue member function called" << std::endl;
+    return this->_value;
+}
+
+void Fixed::setStoredValue(int const raw)
+{
+    this->_value = raw;
+}
